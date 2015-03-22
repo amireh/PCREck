39703901fd9475f7f79e7cf63a311d9b95cefb90
Ahmad Amireh <ahmad.amireh@gmail.com>
2015-03-22 11:08:12 -0600

    Removed built output

--- ui/config/codemirror.js
+++ ui/config/codemirror.js
@@ -1,6 +1,7 @@
 var CodeMirror = require('codemirror');
 // var theme = 'zenburn';
 
+// require('codemirror/mode/javascript/javascript');
 require('codemirror/addon/display/placeholder');
 require('codemirror/addon/selection/mark-selection');
 // require('style!css!codemirror/theme/' + theme + '.css');
@@ -9,6 +10,8 @@ require('codemirror/addon/selection/mark-selection');
 CodeMirror.defaults.lineNumbers = false;
 CodeMirror.defaults.dragDrop = false;
 CodeMirror.defaults.styleSelectedText = true;
+// CodeMirror.defaults.matchBrackets = true;
+// CodeMirror.defaults.mode = 'javascript';
 CodeMirror.defaults.extraKeys = {
   'Tab': false,
   'Shift-Tab': false
